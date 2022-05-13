84cf742a262202d7044cf06346e778ec1387c599
giorgio <giorgiolink94@hotmail.it>
2022-05-13 11:22:19 +0100

    update 1

--- src/slices/favorite/favoriteSlice.js
+++ src/slices/favorite/favoriteSlice.js
@@ -1,5 +1,15 @@
-const favoriteSlice = () => {
-  return <div>favoriteSlice</div>;
-};
+import { createSlice } from "@reduxjs/toolkit";
+const favoriteSlice = createSlice({
+  name: "favorite",
+  initialState: { songs: [] },
+  reducers: {
+    addSongs: (state, actions) => {
+      return {
+        songs: [...state.songs, actions.payload],
+      };
+    },
+  },
+});
 
-export default favoriteSlice;
+export default favoriteSlice.reducer;
+export const { addSong } = favoriteSlice.reducer;
